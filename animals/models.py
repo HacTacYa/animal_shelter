0566c5f3bd75bf0c388552a63d45d3433a5bf406
HacTacYa <hactacyame@yandex.ru>
2024-06-20 15:33:00 +0700

    Add animals app and models

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    breed = models.CharField(max_length=100)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='animals/')
+    health_status = models.CharField(max_length=100)
+    is_adopted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Adoption(models.Model):
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    adopter_name = models.CharField(max_length=100)
+    adoption_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.adopter_name} adopted {self.animal.name}"
